ddae33ba1276447b488e834cedd9eb18d081839a
timo raty <ratytimo@gmail.com>
2016-10-17 20:14:15 +0300

    CLEANUP

--- test/TestAll.cpp
+++ test/TestAll.cpp
@@ -5,26 +5,14 @@
  *      Author: timppa
  */
 
-#include <string>
+//#include <string>
 
 #include "gtest/gtest.h"
 
-//#define ARG_INCL_PERFORMANCE "--performance"
-//
-//namespace testargs {
-//	bool isPerfIncluded;
-//}
-//
 int main(int argc,char** argv)
 {
 	int rc=-1;
 
-//	testargs::isPerfIncluded = false;
-//	for ( int i=1; i<argc; i++ ){
-//		if ( 0==strcmp(argv[i],ARG_INCL_PERFORMANCE) ){
-//			testargs::isPerfIncluded = true;
-//		}
-//	}
 	::testing::InitGoogleTest(&argc,argv);
 	rc=RUN_ALL_TESTS();
 	return rc;
